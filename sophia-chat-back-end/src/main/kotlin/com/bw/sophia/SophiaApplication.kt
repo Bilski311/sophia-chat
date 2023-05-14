8e89a8b8e668833e4d39eb770bd8d910c45f121f
Dominik Bilski <dominik.bilski24@gmail.com>
2023-05-14 23:17:41 +0200

    Initialize backend.

---
+++ sophia-chat-back-end/src/main/kotlin/com/bw/sophia/SophiaApplication.kt
@@ -0,0 +1,11 @@
+package com.bw.sophia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication()
+class SophiaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SophiaApplication>(*args)
+}
---
+++ sophia-chat-back-end/src/test/kotlin/com/bw/sophia/SophiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bw.sophia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SophiaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
