98c03db694ed7a409e772edfca4667fe70e8b603
Dominik Bilski <dominik.bilski24@gmail.com>
2023-05-16 20:03:19 +0200

    [BE] Add file upload endpoint.

---
+++ sophia-chat-back-end/src/main/kotlin/com/bw/sophia/file/FileUploadController.kt
@@ -0,0 +1,17 @@
+package com.bw.sophia.file
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+class FileUploadController {
+    @PostMapping("/upload")
+    fun handleFileUpload(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
+        return ResponseEntity.ok("File uploaded successfully, filename: ${file.originalFilename}")
+    }
+}
+
+
+
