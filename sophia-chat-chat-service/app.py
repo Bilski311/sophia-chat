240e0c99b3fd06f5e4270354a33be3b1c4ea5ac5
Dominik Bilski <dominik.bilski24@gmail.com>
2023-05-18 23:16:49 +0200

    [CS] Make answer endpoint usable.

--- sophia-chat-chat-service/app.py
+++ sophia-chat-chat-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from qdrant_client import QdrantClient
 from llama_index.vector_stores import QdrantVectorStore
 from llama_index import GPTVectorStoreIndex, StorageContext, Document
@@ -21,21 +21,24 @@ def upload_file():
 
     return 'File uploaded successfully'
 
-@app.route('/index', methods=['GET'])
-def get_index():
+@app.route('/index/<collection_name>', methods=['POST'])
+def get_answer(collection_name):
+    data = request.get_json()
+
+    query = data.get("query")
+    if query is None:
+        return jsonify({"error": "Please provide 'query'"}), 400
+
     vector_store = QdrantVectorStore(
         client=client,
-        collection_name="file",
+        collection_name=collection_name,
     )
-    storage_context = StorageContext.from_defaults(
-        vector_store = vector_store
-    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
     index = GPTVectorStoreIndex.from_documents([], storage_context=storage_context)
     query_engine = index.as_query_engine()
-    response = query_engine.query("Who is Dominik Bilski?")
-    print(response)
+    response = query_engine.query(query)
 
-    return 'Success'
+    return jsonify({"response": response}), 200
 
 if __name__ == '__main__':
     app.run()
